58674167fb21fbcc09455e1609b0d12511835ce0
abhilashwebnapp <abhilash@ordant.com>
2018-01-14 10:58:09 +0530

    Flash messenger service

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { FlashMessageService } from './services/flash-message/flash-message.service';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    FlashMessageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
